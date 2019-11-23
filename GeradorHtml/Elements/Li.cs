ea16a221a34bf85fc51f0133e9be3e34f8bb9be1
Alexandre da Hora do Sacramento <ra00267335@pucsp.edu.br>
2019-11-23 16:16:45 -0200

    dois novos elementos e adicao do gitignore

---
+++ GeradorHtml/Elements/Li.cs
@@ -0,0 +1,12 @@
+﻿using GeradorHtml.Decorators;
+
+namespace GeradorHtml.Elements
+{
+    public class Li : DecoratorElement
+    {
+        public override string GetTagName()
+        {
+            return "li";
+        }
+    }
+}
---
+++ GeradorHtml/Elements/Ul.cs
@@ -0,0 +1,12 @@
+﻿using GeradorHtml.Decorators;
+
+namespace GeradorHtml.Elements
+{
+    public class Ul : DecoratorElement
+    {
+        public override string GetTagName()
+        {
+            return "ul";
+        }
+    }
+}
